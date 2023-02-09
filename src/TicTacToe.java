ea97bbc35035c75e454a8e37a63e4ae12599bc10
Onischenka <onischenka@gmail.com>
2023-02-09 19:10:25 +0200

    Code refactoring

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -11,172 +11,183 @@ public class TicTacToe implements ActionListener {
 	JPanel button_panel = new JPanel();
 	JLabel textfield = new JLabel();
 	JButton[] buttons = new JButton[9];
-	boolean player_turn;
-	boolean gameover = false;
-	
-	TicTacToe(){
+	boolean player_turn, isComputer = true;
+
+	TicTacToe() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,400);
-		frame.getContentPane().setBackground(new Color(99,57,116));
+		frame.setSize(400, 400);
+//	frame.getContentPane().setBackground(new Color(99,57,116));
 		frame.setLayout(new BorderLayout());
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-		textfield.setBackground(new Color(60,60,60));
-		textfield.setForeground(new Color(25,250,0));
-		textfield.setFont(new Font("Comic Sans MS",Font.BOLD,22));
+
+		textfield.setBackground(new Color(60, 60, 60));
+		textfield.setForeground(new Color(25, 250, 0));
+		textfield.setFont(new Font("Comic Sans MS", Font.BOLD, 22));
 		textfield.setHorizontalAlignment(JLabel.CENTER);
 		textfield.setText("Хрестики-Нолики");
 		textfield.setOpaque(true);
 		title_panel.setLayout(new BorderLayout());
-		title_panel.setBounds(0,0,800,100);
-		
-		button_panel.setLayout(new GridLayout(3,3));
-		//button_panel.setBackground(new Color(150,150,150));
-		
-		for(int i=0;i<9;i++) {
+//		title_panel.setBounds(0,0,800,100);
+
+		button_panel.setLayout(new GridLayout(3, 3));
+		// button_panel.setBackground(new Color(150,150,150));
+
+		for (int i = 0; i < 9; i++) {
 			buttons[i] = new JButton();
 			button_panel.add(buttons[i]);
-			buttons[i].setFont(new Font("MV Boli", Font.BOLD,120));
+			buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
 			buttons[i].setFocusable(false);
-			buttons[i].setBackground(new Color(99,57,116));
+			buttons[i].setBackground(new Color(99, 57, 116));
 			buttons[i].setEnabled(false);
 			buttons[i].addActionListener(this);
-			
+
 		}
-		
+
 		title_panel.add(textfield);
 		frame.add(title_panel, BorderLayout.NORTH);
 		frame.add(button_panel);
-		
+
 		firstTurn();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		for(int i=0;i<9;i++){
-			if(e.getSource()==buttons[i]) {
-				if(player_turn) {
-					if( buttons[i].getText()=="") {
-						buttons[i].setForeground(new Color(0,0,225));
-						buttons[i].setText("X");						
-						textfield.setText("Хід 0");		
-						check();
-						player_turn = false;
-					} 
-				} else {
-					if(buttons[i].getText()=="") {
-						buttons[i].setForeground(new Color(225,0,0));
-						buttons[i].setText("0");						
-						textfield.setText("Хід X");
-						check();
-						player_turn = true;
-					} 
+
+		for (int i = 0; i < 9; i++) {
+			if (e.getSource() == buttons[i]) {
+				if (buttons[i].getText() == "") {
+					if (player_turn) {
+						turnx(i);
+					} else {
+						turn0(i);
+					}
+
 				}
+
+				if (isComputer) {
+					comuterPlaying();
+				}
+
 			}
 		}
-		
 	}
-	
+
 	public void firstTurn() {
-		
+
 //виводимо назву та починаємо гру
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-/*активуємо кнопки для початку гри, якщо цього не робити, 
-то гравець може почати гру до того як згенерується рандомно 
-хто перший починає гру
-*/
-		for(int i=0;i<9;i++) {
+		/*
+		 * активуємо кнопки для початку гри, якщо цього не робити, то гравець може
+		 * почати гру до того як згенерується рандомно хто перший починає гру
+		 */
+		for (int i = 0; i < 9; i++) {
 			buttons[i].setEnabled(true);
 		}
-		
-		if(random.nextInt(2)==0) {
+
+		if (random.nextInt(2) == 0) {
 			player_turn = false;
 			textfield.setText("Хід 0");
 		} else {
 			player_turn = true;
 			textfield.setText("Хід X");
 		}
-	
+
 	}
-	
-	public void check() {
-		int countFilledButtons=0;
-		
-		gameover = check_buttons_group(0,1,2,"X");
-		gameover = check_buttons_group(3,4,5,"X");
-		gameover = check_buttons_group(6,7,8,"X");		
-		gameover = check_buttons_group(0,3,6,"X");
-		gameover = check_buttons_group(1,4,7,"X");
-		gameover = check_buttons_group(2,5,8,"X");
-		gameover = check_buttons_group(0,4,8,"X");
-		gameover = check_buttons_group(2,4,6,"X");
 
-		gameover = check_buttons_group(0,1,2,"0");
-		gameover = check_buttons_group(3,4,5,"0");
-		gameover = check_buttons_group(6,7,8,"0");	
-		gameover = check_buttons_group(0,3,6,"0");
-		gameover = check_buttons_group(1,4,7,"0");
-		gameover = check_buttons_group(2,5,8,"0");
-		gameover = check_buttons_group(0,4,8,"0");
-		gameover = check_buttons_group(2,4,6,"0");
+//	хід за хрестики
+	public void turnx(int i) {
+		buttons[i].setForeground(new Color(0, 0, 225));
+		buttons[i].setText("X");
+		textfield.setText("Хід 0");
+		checkWinner();
+		player_turn = false;
+	}
 
-/*нічія може бути коли всі кнопки заповнені та відсутня 
-  переможна комбінація
-*/		
-		for(;countFilledButtons<9;countFilledButtons++) {
-			if (buttons[countFilledButtons].getText()=="") {
+//	хід за нулики
+	public void turn0(int i) {
+		buttons[i].setForeground(new Color(225, 0, 0));
+		buttons[i].setText("0");
+		textfield.setText("Хід X");
+		checkWinner();
+		player_turn = true;
+	}
+
+	public void checkWinner() {
+		int fillButtons = 0;
+
+		/*
+		 * нічия може бути коли всі кнопки заповнені та відсутня переможна комбінація
+		 */
+		for (fillButtons = 0; fillButtons < 9; fillButtons++) {
+			if (buttons[fillButtons].getText() == "") {
 				break;
-			}			
+			}
 		}
-		
-		if(countFilledButtons==9 && gameover==false) {
+//	якщо всі клітинки заповнені то може бути нічия
+		if (fillButtons == 9) {
 			Draw();
 		}
-		
+//перевіряємо що група кнопок має переможну комбінацію			
+		checkWinnerCombination(0, 1, 2);
+		checkWinnerCombination(3, 4, 5);
+		checkWinnerCombination(6, 7, 8);
+		checkWinnerCombination(0, 3, 6);
+		checkWinnerCombination(1, 4, 7);
+		checkWinnerCombination(2, 5, 8);
+		checkWinnerCombination(0, 4, 8);
+		checkWinnerCombination(2, 4, 6);
+
 	}
-	
-	public boolean check_buttons_group(int first, int second, int third, String turn) {
-		boolean gameover = false;
-		if( buttons[first].getText()==turn  &&
-			buttons[second].getText()==turn &&
-			buttons[third].getText()==turn  )
-			{
-				Wins(first,second,third,this.player_turn);
-				gameover = true;
-			}
-		
-		return gameover;
+
+	public void checkWinnerCombination(int first, int second, int third) {
+
+		if (buttons[first].getText() == "X" && buttons[second].getText() == "X" && buttons[third].getText() == "X"
+				|| buttons[first].getText() == "0" && buttons[second].getText() == "0"
+						&& buttons[third].getText() == "0") {
+			Wins(first, second, third);
+		}
+
 	}
-	
-	public void Wins(int first, int second, int third, boolean player) {
+
+	public void Wins(int first, int second, int third) {
 		buttons[first].setBackground(Color.GREEN);
 		buttons[second].setBackground(Color.GREEN);
 		buttons[third].setBackground(Color.GREEN);
-		
-		for(int i=0;i<9;i++) {
+
+		for (int i = 0; i < 9; i++) {
 			buttons[i].setEnabled(false);
 		}
-		if(player) {
+		if (player_turn) {
 			textfield.setText("Перемогли X");
 		} else {
-			textfield.setText("Перемогли 0");		
+			textfield.setText("Перемогли 0");
 		}
-		
+		isComputer = false;
 	}
-	
-	public void Draw() {		
-		for(int i=0;i<9;i++) {
+
+	public void Draw() {
+		for (int i = 0; i < 9; i++) {
 			buttons[i].setEnabled(false);
 		}
-			textfield.setText("Нічия");		
+		textfield.setText("Нічия");
 	}
-	
+
+	public void comuterPlaying() {
+		for (int i = 0; i < 9; i++) {
+			if (buttons[i].getText() == "") {
+				if (player_turn) {
+					turnx(i);
+				} else {
+					turn0(i);
+				}
+				break;
+			}
+		}
+	}
 }
